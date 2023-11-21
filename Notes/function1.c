a656d7b14645142d6b40d364b906b149173c883a
Shoyeb45 <shoyebff45@gmail.com>
2023-11-21 11:21:22 +0530

    new files

--- Notes/function1.c
+++ Notes/function1.c
@@ -1,1 +1,13 @@
-#inlcude<stdio.h>
+//1.function with with argument and wit return type.
+
+#include<stdio.h>
+int add(int,int);
+
+void main()
+{
+   printf("%d",add(2,56));
+}
+
+int add(int a , int b){
+    return a+b;
+}
