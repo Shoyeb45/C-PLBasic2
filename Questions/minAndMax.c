55264ca3cfd3f6738a129a11eebc013025e8cb25
Shoyeb45 <shoyebff45@gmail.com>
2023-11-22 10:04:59 +0530

    new file

---
+++ Questions/minAndMax.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int max(int ,int);
+void main()
+{
+    int a,b,c,d,e;
+    printf("Enter the five numbers:");
+    scanf("%d%d%d%d%d", &a ,&b ,&c ,&d ,&e);
+    
+    int Max = max(e,max(d,(max(c,max(a,b))))) ;
+    int Min = min(e,min(d,(min(c,min(a,b))))) ;
+    printf("Maximu value is %d and minimum is %d",Max,Min);
+    //one way is to write the conditionals statements for each . now using function
+    
+}
+
+int max(int a, int b){
+    if(a>=b)
+    return a;
+    else
+    return b;
+}
+int min(int a, int b){
+    if(a<=b)
+    return a;
+    else
+    return b;
+}
--- Questions/series1.c
+++ Questions/series1.c
@@ -2,11 +2,11 @@
 
 void main()
 {
-    int n , sum =0;
+        int n , sum =0;
     printf("Enter the value of n(number of terms):");
     scanf("%d",& n);
 
-    for (int i=12 ; i<=(12*n) ; i+=12){
+    for (int i=110 ; i<=(100*n+10) ; i+=100){
         sum=sum+i;
 
         printf("%d ",i);
