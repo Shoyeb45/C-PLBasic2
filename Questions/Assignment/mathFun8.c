5d549b04e0824704a355fb0337b0497ca7f52d5e
Shoyeb45 <shoyebff45@gmail.com>
2023-11-23 19:11:21 +0530

    new file

--- Questions/Assignment/mathFun8.c
+++ Questions/Assignment/mathFun8.c
@@ -1,1 +1,19 @@
+#include<stdio.h>
 
+int function(int);
+
+void main()
+{
+    int x;
+    printf("f(x)=10x+10\n");
+    printf("Enter value of x to get the f(x) at that point.");
+    scanf("%d", & x);
+
+    int c=function(x);
+    printf("f(%d)=%d",x,c);
+
+}
+
+int function(int a){
+    return 10*a+10;
+}
