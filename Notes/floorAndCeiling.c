69903c60e7c62086ac2acf57bf629d88b567eb17
Shoyeb45 <shoyebff45@gmail.com>
2023-12-03 15:26:15 +0530

    new

--- Notes/floorAndCeiling.c
+++ Notes/floorAndCeiling.c
@@ -1,10 +1,14 @@
     #include<stdio.h>
-
+#include<math.h>
     void main()
     {
-        int n;
+        float n;
         printf("Enter the number:");
         scanf("%f",&n);
+        float c =floor(n);
+        float t= ceil(n);
+        
+        printf("%f %f",c , t);
         
            
 }
