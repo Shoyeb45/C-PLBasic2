6ab894b0ce2c760804b87eca5164142b08fa28dd
Shoyeb45 <shoyebff45@gmail.com>
2023-12-05 16:24:24 +0530

    multiple compiling

--- Notes/Multiple Compiling/main.c
+++ Notes/Multiple Compiling/main.c
@@ -1,6 +1,12 @@
 #include<stdio.h>
+#include"header.h"
 
 void main()
 {
+    int a,b;
+    printf("Enter two numbers:");
+    scanf("%d %d",&a,&b);
+
+    printf("\nAddition %d and Multiplication:",add(a,b), mult(a,b));
   
 }
