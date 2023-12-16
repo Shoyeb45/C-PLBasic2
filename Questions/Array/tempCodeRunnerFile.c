0cafbd1271d475aeaa91cb89013b89f0d4119aae
Shoyeb45 <shoyebff45@gmail.com>
2023-12-16 19:49:28 +0530

    new question

--- Questions/Array/tempCodeRunnerFile.c
+++ Questions/Array/tempCodeRunnerFile.c
@@ -1,5 +1,16 @@
-     int a[n];
-
-    for(int i=0 ; i<n ; i++){
-        scanf("%d",&a[i]);
-    }
+        for(int i=0; i<10 ; i++){
+//                 int count=0;
+//                 for(int j=i+1 ; j<10 ; j++){
+//                         if(Union[i] == Union[j] && Union[j] != (int)&A[4] ){
+//                                 Union[j] = (int)&A[4];
+//                              ++count;
+//         }
+//                 }
+//                     if(count)
+//                     printf("%d ", Union[i]);
+//                     else{
+//                     if(Union[i] != (int)&A[4])
+//                     printf("%d ", Union[i]);
+// }
+                
+//         }
