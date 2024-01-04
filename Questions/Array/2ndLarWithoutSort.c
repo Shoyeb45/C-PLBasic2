2fbbd4733964316cbbc4e26fa2fafda6a5fdf50d
Shoyeb45 <shoyebff45@gmail.com>
2024-01-04 22:42:55 +0530

    Finding second largest element in array

---
+++ Questions/Array/2ndLarWithoutSort.c
@@ -0,0 +1,34 @@
+//Finding second largest element in array
+
+#include<stdio.h>
+
+int main()
+{
+    int a[5] = {1, 12, 3, -9, 23};
+
+int lar = 0;  int j;
+    for(int i=0; i<5 ; i++){
+        if(lar < a[i]){
+            lar = a[i];
+            j = i;
+        }
+    }
+
+    //For second largest
+
+    int large2nd = 0;
+
+    for(int i=0 ; i<5 ; i++){
+        if(i == j)
+        {
+
+        }
+        else{
+            if (large2nd < a[i]){
+                large2nd = a[i];
+            }
+        }
+    }
+
+    printf("Second largest is:%d",large2nd);
+}
